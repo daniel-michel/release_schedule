ac8eb6ac12aa8d73f68137d5fcde764f49a8b173
daniel-michel <65034538+daniel-michel@users.noreply.github.com>
2023-11-16 15:09:25 +0100

    fix: remove unused import

--- test/view/movie_item_test.dart
+++ test/view/movie_item_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:release_schedule/model/date_format.dart';
 import 'package:release_schedule/model/movie.dart';
 import 'package:release_schedule/view/movie_item.dart';
 
